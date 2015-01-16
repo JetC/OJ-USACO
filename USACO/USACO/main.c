4d9c6e4c0c577f805cfc1a89924edb7af5e4e109
Sun PeiFeng <g7733pf@gmail.com>
2015-01-16 11:30:05 +0800

    Ride OK

--- USACO/USACO/main.c
+++ USACO/USACO/main.c
@@ -14,24 +14,30 @@ int main () {
     FILE *fout = fopen ("ride.out", "w");
     char a[7];
     char b[7];
-    fscanf (fin, "%s\n%s", a,b);	/* the two input integers */
+    fscanf (fin, "%s %s", a,b);	/* the two input integers */
+    printf("a:%s, b:%s\n",a,b);
     long result1 = 1;
     long result2 = 1;
     int j = 0;
     short aDidEnd = 0;
     short bDidEnd = 0;
-    while (!(aDidEnd && bDidEnd)) {
-        if (a[j] == '\n' || aDidEnd == 1) {
+    while (j<7) {
+        if (a[j] == '\0' || aDidEnd == 1) {
             aDidEnd = 1;
+            printf("aEnded");
         }
         else{
             result1 *= (a[j]-64);
+            printf("a[j]-64 = %d\n",a[j]-64);
+
         }
-        if (b[j] == '\n' || bDidEnd == 1) {
+        if (b[j] == '\0' || bDidEnd == 1) {
             bDidEnd = 1;
+            printf("bEnded");
         }
         else{
             result2 *= (b[j]-64);
+            printf("b[j]-64 = %d\n",b[j]-64);
         }
         j++;
     }
