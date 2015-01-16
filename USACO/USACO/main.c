5f29a3422473e4dc6f1e176da43491d101da7ffb
Sun PeiFeng <g7733pf@gmail.com>
2015-01-16 10:07:56 +0800

    tmp using char[7]

---
+++ USACO/USACO/main.c
@@ -0,0 +1,50 @@
+/*
+ ID: g7733pf1
+ LANG: C
+ TASK: ride
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+
+    FILE *fin = fopen ("ride.in", "r");
+    FILE *fout = fopen ("ride.out", "w");
+    char a[7];
+    char b[7];
+    fscanf (fin, "%s\n%s", a,b);	/* the two input integers */
+    long result1 = 1;
+    long result2 = 1;
+    int j = 0;
+    short aDidEnd = 0;
+    short bDidEnd = 0;
+    while (!(aDidEnd && bDidEnd)) {
+        if (a[j] == '\n' || aDidEnd == 1) {
+            aDidEnd = 1;
+        }
+        else{
+            result1 *= (a[j]-64);
+        }
+        if (b[j] == '\n' || bDidEnd == 1) {
+            bDidEnd = 1;
+        }
+        else{
+            result2 *= (b[j]-64);
+        }
+        j++;
+    }
+    
+    if ((result1%47)==(result2%47)) {
+        printf("GO\n");
+        fprintf (fout, "%s\n", "GO");
+    }
+    else
+    {
+        printf("STAY\n");
+        fprintf (fout, "%s\n", "STAY");
+    }
+
+    return 0;
+}
