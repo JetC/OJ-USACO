b409cb839b74241e7a4a57a2fab531d36ea16842
Sun PeiFeng <g7733pf@gmail.com>
2015-01-18 22:19:31 +0800

    example reading file line by line

--- USACO/USACO/main.c
+++ USACO/USACO/main.c
@@ -4,53 +4,31 @@
  TASK: ride
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
 
-int main () {
+struct stack {
+    int *next;
+    char name;
+};
 
-    FILE *fin = fopen ("ride.in", "r");
-    FILE *fout = fopen ("ride.out", "w");
-    char a[7];
-    char b[7];
-    fscanf (fin, "%s %s", a,b);	/* the two input integers */
-    printf("a:%s, b:%s\n",a,b);
-    long result1 = 1;
-    long result2 = 1;
-    int j = 0;
-    short aDidEnd = 0;
-    short bDidEnd = 0;
-    while (j<7) {
-        if (a[j] == '\0' || aDidEnd == 1) {
-            aDidEnd = 1;
-            printf("aEnded");
-        }
-        else{
-            result1 *= (a[j]-64);
-            printf("a[j]-64 = %d\n",a[j]-64);
-
-        }
-        if (b[j] == '\0' || bDidEnd == 1) {
-            bDidEnd = 1;
-            printf("bEnded");
-        }
-        else{
-            result2 *= (b[j]-64);
-            printf("b[j]-64 = %d\n",b[j]-64);
-        }
-        j++;
+int main()
+{
+    FILE *ptr_file;
+    const int size = 1000;
+    char buf[size];
+    
+    
+    
+    ptr_file = fopen("ride.in","r");
+    if (!ptr_file)
+        return 1;
+    
+    while (fgets(buf,size, ptr_file) != NULL){
+        printf("%s",buf);
     }
     
-    if ((result1%47)==(result2%47)) {
-        printf("GO\n");
-        fprintf (fout, "%s\n", "GO");
-    }
-    else
-    {
-        printf("STAY\n");
-        fprintf (fout, "%s\n", "STAY");
-    }
-
+    printf("\n");
+    
+    fclose(ptr_file);
     return 0;
 }
